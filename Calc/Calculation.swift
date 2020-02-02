cb799e01c631f988ccddd4bc5cf246e441c11914
Ali Safari <cpt.safari@gmail.com>
2020-02-03 01:45:10 +0330

    Preparing the basics of MVC calc project

---
+++ Calc/Calculation.swift
@@ -0,0 +1,24 @@
+//
+//  Calculation.swift
+//  Calc
+//
+//  Created by Ali Safari on 2/3/20.
+//  Copyright © 2020 Ali Safari. All rights reserved.
+//
+
+import Foundation
+
+class Calculation {
+    
+    var result: Int = 0
+    
+    func addition(input1: Int, input2: Int) -> Int {
+        result = input1 + input2
+        return result
+    }
+    
+    //init(result: Int) {
+    //    self.result = result
+    //}
+    
+}
--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var calculation = Calculation()
+    
+    var input1 = Int()
+    var gotInput1 = false
+    var input2 = Int()
 
+    @IBOutlet weak var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func number(_ sender: UIButton) {
+        displayLabel.text?.append("1")
+    }
+    @IBAction func add(_ sender: UIButton) {
+        if displayLabel.text != nil || displayLabel.text != "" {
+            if gotInput1 == false {
+                input1 = Int(displayLabel.text!)!
+                displayLabel.text = ""
+                gotInput1 = true
+            } else {
+                input2 = Int(displayLabel.text!)!
+                displayLabel.text = ""
+            }
+        }
+    }
+    @IBAction func equal(_ sender: UIButton) {
+        if displayLabel.text != nil || displayLabel.text != "" {
+            input2 = Int(displayLabel.text!)!
+            displayLabel.text = ""
+        }
+        
+        
+        print("I1:",input1)
+        print("I2:", input2)
+        print(String(calculation.addition(input1: input1, input2: input2)))
+        displayLabel.text = String(calculation.addition(input1: input1, input2: input2))
+    }
+    
 }
 
